85722befd9a1afad52da4d27de31805a209e26ca
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2017-03-24 18:01:44 +0100

    Solution

--- Gruntfile.part.js
+++ Gruntfile.part.js
@@ -1,3 +1,29 @@
 /*jshint camelcase:false*/
 
-module.exports = {};
+module.exports = {
+    jshint: {
+        options: {
+            jshintrc: '.jshintrc'
+
+        },
+        verify: {
+            options: {
+                jshintrc: true,
+                reporter: 'checkstyle',
+                reporterOutput: 'target/jshint.xml'
+            },
+            files: {src: ['app/**/*.js', 'test/**/*.js', '!app/bower_components/**/*.js']}
+        }
+    },
+    karma: {
+        unit: {
+            configFile: 'test/karma.conf.js'
+        }
+    },
+    wiredep: {
+        task: {
+            src: ['app/*.html'],
+            exclude: ['app/bower_components/bootstrap/dist/js']
+        }
+    }
+};
